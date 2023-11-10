7b071b4ec9b505db5e279c197a8eb25e8113284c
Shellykauppa / Antti <antti@shellykauppa.fi>
2023-11-11 00:13:16 +0200

    Replace the Shelly.call to PLUGS_UI with rpc call to localhost.
    Works with in 1.0.x firmware.
    
    Refers #2

--- shelly/index.js
+++ shelly/index.js
@@ -210,7 +210,11 @@ function configureLight(shellyRGB, onBrightness, offBrightness) {
     offColorConfig.brightness = offBrightness;
     uiConfig.leds.colors["switch:0"].off = offColorConfig;
 
-    Shelly.call( "PLUGS_UI.SetConfig", { config: uiConfig }, reportConfigError )
+    // This call would fail in firmware 1.0.0-beta up to at least 1.0.8:
+    // Shelly.call( "PLUGS_UI.SetConfig", { config: uiConfig }, reportConfigError )
+    // Hence, using the rpc api to achieve the same.
+    let configUrl = "http://localhost/rpc/PLUGS_UI.SetConfig?config=" + JSON.stringify(uiConfig);
+    Shelly.call("HTTP.Request", { method: "GET", url: configUrl, timeout: 15, ssl_ca: "*" }, reportConfigError);
   }
   else {
     console.log("PPI: Incorrect color mode (", uiConfig.leds.mode, "), should be \"switch\"");
